f5126b2778b1cf7774bafe19fef53f14610b5b9a
Nicole Mills <nam0068@auburn.edu>
2022-03-12 22:02:57 -0600

    Added second num input

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -6,10 +6,12 @@ namespace TwoNumbers
     {
         static void Main(string[] args)
         {
-            int num1, num2, result // variables
-                // read user input numbers and convert to integers
-                Console.Write("Please enter your first number: ");
-                num1 = Convert.ToInt32(Console.ReadLine());
+            int num1, num2, result; // variables
+            // read user input numbers and convert to integers
+            Console.Write("Please enter your first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter your second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
