ad4bfa41950f41c3d79a8514e61c8e59380ef60d
Nicole Mills <nam0068@auburn.edu>
2022-03-12 22:17:08 -0600

    Updated main program if else statement for user input

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -7,11 +7,37 @@ namespace TwoNumbers
         static void Main(string[] args)
         {
             int num1, num2, result; // variables
+
             // read user input numbers and convert to integers
             Console.Write("Please enter your first number: ");
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Please enter your second number: ");
             num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Select an mathematical operation
+            Console.WriteLine("Select an operation: 1. Addition, 2. Subtraction, or 3. Multiplication: ");
+            int operation = Convert.ToInt32(Console.ReadLine());
+
+            // Math operation selection
+            if (operation == 1)
+            {
+                result = num1 + num2;
+                Console.WriteLine("Addition selected. Sum = " + result.ToString());
+            }
+
+            else if (operation == 2)
+            {
+                result = num1 - num2;
+            }
+
+            else if (operation == 3)
+            {
+                result = num1 * num2;
+            }
+
+            else
+                Console.Write("Invalid input.");
+            Console.ReadLine();
         }
     }
 }
