1b03a26b018c78721afd99ddc40ea60865442771
Nicole Mills <nam0068@auburn.edu>
2022-03-12 21:59:44 -0600

    Created FirstConsoleApplication program and added user input into main program

--- FirstConsoleApplication/Program.cs
+++ FirstConsoleApplication/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result // variables
+                // read user input numbers and convert to integers
+                Console.Write("Please enter your first number: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
